102e5ce264e8c8e833a123c3c8f72c876e7a1539
Oana_Juravle <oana.juravle96@gmail.com>
2019-06-17 22:36:46 +0300

    update dist

--- dist/index.js
+++ dist/index.js
@@ -1,49 +1,60 @@
 "use strict";
 
-var _generalImports = require("./src/templates/generalImports");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.generateTests = generateTests;
 
-var _generalImports2 = _interopRequireDefault(_generalImports);
-
-var _getFiles = require("./src/getFiles");
+var _getFiles = require("./getFiles");
 
 var _getFiles2 = _interopRequireDefault(_getFiles);
 
-var _renderSuiteSchema = require("./src/templates/renderSuiteSchema");
-
-var _renderSuiteSchema2 = _interopRequireDefault(_renderSuiteSchema);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-require('@babel/register');
-
+// require('@babel/register');
+// import generalImports from './templates/generalImports';
+// // import renderTestSuite from './templates/renderSuiteSchema';
 var path = require('path');
 
 var fs = require('fs');
 
-window.TEST_GENERATOR_WARNINGS = [];
-var fileName = process.argv[2];
-var matchedFiles = [];
-var projectEntry = 'D:\\GitHub\\react-app';
-console.log(projectEntry); // // TODO: decide whether or not I should use src here - maybe ask for entry point?
-
-var files = (0, _getFiles2["default"])("".concat(projectEntry, "\\src"), matchedFiles, fileName);
-console.log('matchedFiles', matchedFiles);
-global.console.log('MATCHED FILES', matchedFiles);
-var componentPath = files[0];
-var destination = componentPath.split('/').slice(-1)[0].split('.')[0];
-var destinationFile = "".concat(projectEntry, "tests\\").concat(destination, ".test.js");
-fs.writeFile(destinationFile, (0, _generalImports2["default"])(componentPath), function (err) {
-  if (err) throw err;
-  console.log('Added imports');
-});
-fs.appendFile(destinationFile, (0, _renderSuiteSchema2["default"])(componentPath), function (err) {
-  if (err) throw err;
-  console.log('Added test suite');
-});
-
-try {
-  fs.writeFileSync('./src/testGeneratorWarnings.js', "module.exports = ".concat(JSON.stringify(window.TEST_GENERATOR_WARNINGS)));
-  console.log('Added warnings');
-} catch (err) {
-  throw err;
-}
+function generateTests() {
+  console.log('**** From Package ---- ****');
+  var rootDir = path.resolve(__dirname, './src');
+  console.log('rootDir', rootDir);
+  var matchedFiles = [];
+  var files = (0, _getFiles2["default"])(rootDir, matchedFiles, 'UsersIndex');
+  console.log(files);
+} // module.exports = { generateTests };
+// window.TEST_GENERATOR_WARNINGS = [];
+// var fileName = process.argv[2];
+// const matchedFiles = [];
+// const projectEntry = 'D:\\GitHub\\react-app';
+// console.log(projectEntry);
+// // // TODO: decide whether or not I should use src here - maybe ask for entry point?
+// const files = getFiles(`${projectEntry}\\src`, matchedFiles, fileName);
+// console.log('matchedFiles', matchedFiles);
+// global.console.log('MATCHED FILES', matchedFiles);
+// const componentPath = files[0];
+// const destination = componentPath
+//   .split('/')
+//   .slice(-1)[0]
+//   .split('.')[0];
+// const destinationFile = `${projectEntry}tests\\${destination}.test.js`;
+// fs.writeFile(destinationFile, generalImports(componentPath), err => {
+//   if (err) throw err;
+//   console.log('Added imports');
+// });
+// fs.appendFile(destinationFile, renderTestSuite(componentPath), err => {
+//   if (err) throw err;
+//   console.log('Added test suite');
+// });
+// try {
+//   fs.writeFileSync(
+//     './testGeneratorWarnings.js',
+//     `module.exports = ${JSON.stringify(window.TEST_GENERATOR_WARNINGS)}`,
+//   );
+//   console.log('Added warnings');
+// } catch (err) {
+//   throw err;
+// }
